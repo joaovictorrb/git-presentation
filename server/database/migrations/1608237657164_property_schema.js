deca643488e275d110c2f5f49791c9284431f392
laviny.picoli <laviny.picoli@irriga.net>
2020-12-17 23:16:29 -0300

    Criando tabela properties

--- server/database/migrations/1608237657164_property_schema.js
+++ server/database/migrations/1608237657164_property_schema.js
@@ -7,6 +7,18 @@ class PropertySchema extends Schema {
   up () {
     this.create('properties', (table) => {
       table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('title').notNullable()
+      table.string('address').notNullable()
+      table.decimal('price').notNullable()
+      table.decimal('latitude', 9, 6).notNullable()
+      table.decimal('longitude', 9, 6).notNullable()
       table.timestamps()
     })
   }
